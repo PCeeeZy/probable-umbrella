d20ba37ca934c46302cc9b07d58725f5c055a843
PCwik88 <paul@cwik.us>
2020-05-24 12:09:16 -0500

    kanye env variable

--- controller/kanyeController.js
+++ controller/kanyeController.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 module.exports = {
     getQuote: function(req, res) {
-        return axios.get('http://api.kanye.rest/')
+        // return axios.get('http://api.kanye.rest/')
+        return axios.get(process.env.KANYE)
     }
 }
