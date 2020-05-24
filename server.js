5d2eb57fab2cef666cc5610d641a5d375cb66729
PCwik88 <paul@cwik.us>
2020-05-24 10:43:23 -0500

    server up and running

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-wjkejkjklafdj
+// dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+}
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/umbrella");
+
+app.listen(PORT, () => {
+    console.log(`app started check http://localhost:${PORT}/`);
+})
